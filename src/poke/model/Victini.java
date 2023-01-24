ecc865f1da46b641a388f5891420efd25c7f2eb9
Bryce <bhyer06@Gmail.com>
2023-01-24 08:44:24 -0700

    Created Victini class

---
+++ src/poke/model/Victini.java
@@ -0,0 +1,24 @@
+package poke.model;
+
+public class Victini extends Pokemon
+{
+	
+	public Victini()
+	{
+		super(494, "AshGreninja");
+	}
+	
+	public Victini(String name)
+	{
+		super(494, name);
+		
+	}
+	
+	public Victini(int number, String name)
+	{
+		super(number, name);
+	}
+	
+	
+	
+}
