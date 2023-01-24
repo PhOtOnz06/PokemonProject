7f60ac11d4bd5ba440a0e7c576cd192bb8ff92b7
Bryce <bhyer06@Gmail.com>
2023-01-24 08:39:43 -0700

    Added pokemon.java and set up my ash greninja and gengar classes

---
+++ src/poke/model/AshGreninja.java
@@ -0,0 +1,25 @@
+package poke.model;
+
+public class AshGreninja extends Pokemon
+{
+	
+	public AshGreninja()
+	{
+		super(658, "AshGreninja");
+	}
+	
+	public AshGreninja(String name)
+	{
+		super(658, name);
+		
+	}
+	
+	public AshGreninja(int number, String name)
+	{
+		super(number, name);
+	}
+	
+	
+	
+	
+}
---
+++ src/poke/model/Gengar.java
@@ -0,0 +1,26 @@
+package poke.model;
+
+public class Gengar extends Pokemon
+{
+	
+	public Gengar()
+	{
+		super(94, "Gengar");
+	}
+	
+	public Gengar(String name)
+	{
+		super(94, name);
+	}
+	
+	public Gengar(int number, String name)
+	{
+		super(number, name);
+	}
+	
+	
+	
+	
+	
+	
+}
