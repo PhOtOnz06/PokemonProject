6d8f0d93c326d15a0e0c71a6a72ccf4bb1d945c2
Bryce <bhyer06@Gmail.com>
2023-01-24 08:41:24 -0700

    Added torracat class

---
+++ src/poke/model/Torracat.java
@@ -0,0 +1,24 @@
+package poke.model;
+
+public class Torracat extends Pokemon
+{
+	
+	public Torracat()
+	{
+		super(726, "Torracat");
+	}
+	
+	public Torracat(String name)
+	{
+		super(726, name);
+		
+	}
+	
+	public Torracat(int number, String name)
+	{
+		super(number, name);
+	}
+	
+	
+	
+}
