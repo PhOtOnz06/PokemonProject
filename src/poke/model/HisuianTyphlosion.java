e613d2d80ed3a90047035c7045e8d4d5272ceee8
Bryce <bhyer06@Gmail.com>
2023-01-24 08:43:13 -0700

    Made Hisuian typhlosion class

---
+++ src/poke/model/HisuianTyphlosion.java
@@ -0,0 +1,23 @@
+package poke.model;
+
+public class HisuianTyphlosion extends Pokemon
+{
+	
+	public HisuianTyphlosion()
+	{
+		super(157, "HisuianTyphlosion");
+	}
+	
+	public HisuianTyphlosion(String name)
+	{
+		super(157, name);
+		
+	}
+	
+	public HisuianTyphlosion(int number, String name)
+	{
+		super(number, name);
+	}
+	
+	
+}
